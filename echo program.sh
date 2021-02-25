319daddae781e262e2384d67b129b8baaf81fdcf
Arrayan <57035439+arraysyams@users.noreply.github.com>
2021-02-25 11:06:52 +0700

    More concise explanations
    This feature
    - Tells the user to press ctrl+c to exit
    - Separates explanation for pwd and ls command

--- echo program.sh
+++ echo program.sh
@@ -6,9 +6,11 @@ echo "This is an example of code."
 echo "Main program goes here..."
 
 #Feature request
+echo "This command displays current folder"
 pwd
+echo "This command lists filesystem on current folder"
 ls -lh
-echo "This command displays current folder and lists its filesystem"
 
+echo -e "htop is a simple cli task manager.\nHint: To exit, press ctrl+c"
+sleep 5
 htop
-echo "htop is a simple cli task manager"
